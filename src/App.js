a2cbee49b6a3a3c211ef5f96e1d25b18f7208f67
Ibrahim Mohamed <ibrahim.mohamedm@hotmail.com>
2019-05-11 22:11:39 -0600

    kanye quotes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { withData } from './DataProvider';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  componentDidMount(){
+    this.props.getQuote()
 }
 
-export default App;
+  render() {
+    const styles={
+      quote:{
+        width: "90%",
+        display:"block",
+        margin:"auto",
+        color:"lime",
+        textAlign:"center",
+        textTransform: "capitalize"
+      },
+      button:{
+        display: "block",
+        margin: "auto",
+        marginTop:"1em",
+        zoom: 2.5,
+        backgroundColor: 'white',
+        borderRadius: 5
+      }
+    }
+    return (
+      <div>
+        <h1 style={{color: "gold", textAlign:"center", fontSize: '2.2em'}}>Quotes by Kanye</h1>
+        <button style={styles.button} onClick={() => this.props.getQuote()}>Next</button>
+        <br/>
+        <br/>
+        <h1 style={styles.quote}>"{this.props.quote}"</h1>
+      </div>
+    );
+  }
+}
+
+export default withData(App);
---
+++ src/DataProvider.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import axios from "axios"
+const { Provider, Consumer } = React.createContext()
+
+class DataProvider extends Component {
+    constructor(){
+        super()
+        this.state={
+            quote: ''
+        }
+    }
+
+    getQuote = () => {
+        axios.get(`https://api.kanye.rest`).then(res => {
+            this.setState({
+                quote: res.data.quote
+            })
+        })
+    }
+
+    render() {
+        return (
+            <Provider value={{
+                getQuote: this.getQuote,
+                ...this.state
+            }}
+            >
+                {this.props.children}
+            </Provider>
+        );
+    }
+}
+
+export default DataProvider;
+
+export function withData(C) {
+    return props => <Consumer>{value => <C {...value}{...props} />}</Consumer>
+}
